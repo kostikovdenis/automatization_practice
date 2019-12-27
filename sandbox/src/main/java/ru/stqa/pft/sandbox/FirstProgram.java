05fd1e20abaa92c1cddfe08c1087a829c4c89578
kostikovdenis <kostikovdenis@gmail.com>
2019-12-27 15:25:40 +0300

    Добавлено несколько функций

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.sandbox;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		hello("World");
+		hello("user");
+		hello("Alexey");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со строной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello (String somebody) {
+		 System.out.println("Hello, " + somebody + "!");
+
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+	}
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ru.stqa.pft.sandbox;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
